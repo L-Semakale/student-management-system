78f763936c1b86007da8e1999391ad88cf30b4c3
Mathias-Kabango3 <155973706+Mathias-Kabango3@users.noreply.github.com>
2024-04-17 20:45:22 +0200

    Update views.py

--- Group2/student_management_system/app/views.py
+++ Group2/student_management_system/app/views.py
@@ -6,13 +6,20 @@ from django.contrib.auth import login,logout,authenticate
 from django.contrib.auth.models import User
 from django.contrib import messages
 from .models import Student,Grade,Module
-from .forms import StudentForm
+from .forms import StudentForm,TeacherForm
   
 # Create your views here.
 def index(request):
-  return render(request, 'index.html', {
-    'students': Student.objects.all()
-  })
+  current_user = str(request.user)
+  if current_user.endswith('education.com') or request.user.is_superuser:
+    context = {'students':Student.objects.all()}
+    return render(request,'index.html',context)
+  else:
+    if current_user.endswith('student.com'):
+      data = Student.objects.filter(email=current_user)
+      context = {'students':data}
+      return render(request,'index.html',context)
+  # return render(request, 'index.html',context)
 
 
 def view_student(request, id):
@@ -20,7 +27,6 @@ def view_student(request, id):
 
 # @login_required(login_url='login')
 def add(request):
-  print(request.user)
   if request.method == 'POST' and request.user.is_superuser:
     form = StudentForm(request.POST)
     if form.is_valid():
@@ -53,7 +59,7 @@ def add(request):
 def update(request, id):
   if request.method == 'POST' and request.user.is_superuser:
     student = Student.objects.get(pk=id)
-    form = StudentForm(request.POST, instance=student)
+    form = StudentForm(request.POST,instance=student)
     if form.is_valid():
       form.save()
       return render(request, 'edit.html', {
@@ -69,18 +75,19 @@ def update(request, id):
 
 # @login_required(login_url='login')
 def delete(request, id):
-  if request.method == 'POST'and request.user.is_superuser:
+  if request.method == 'POST'and request.user.is_superuser == True:
     student = Student.objects.get(pk=id)
     student.delete()
   return HttpResponseRedirect(reverse('index'))
 
-def login(request):
+def loginPage(request):
     if request.method == 'POST':
       username = request.POST['username']
       password = request.POST['password']
       user = User.objects.get(username=username)
       user  = authenticate(request,username=username,password=password)
       if user:
+        login(request,user)
         return redirect('index')
     return render(request,'login.html')
 
@@ -90,8 +97,22 @@ def details(request,pk):
   student = Student.objects.get(pk=pk)
   return render(request,'details.html',{'grades':grades,'student':student})
 
+def teacherForm(request):
+  context = {'form':TeacherForm}
+  if request.method == 'POST':
+    form = TeacherForm(request.POST)
+    if form.is_valid():
+      first_name = request.POST['first_name']
+      last_name = request.POST['last_name']
+      email = request.POST['email']
+      password = request.POST['password']
+      faculty = request.POST['faculty']
+      form.save(commit=True)
+      user = User.objects.create_user(username=email,password=password)
+      user.save();
+      return redirect('index')
+  return render(request,'teacher.html',context)
 
 
 
    
-
